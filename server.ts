80632faf3194908c2efbed62a118879fa43b09c3
G-C0der <31240102+G-C0der@users.noreply.github.com>
2023-05-28 14:46:54 +0200

    make heroku able to build and serve static app files

---
+++ server.ts
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000; // Use the port Heroku provides or 3000 locally
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, './build')));
+
+// If no explicit route match, send the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/build/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is up and running on port ${port}`);
+});
