d51d22380336f9f486d0f8e4ba6f8442c401398d
G-C0der <31240102+G-C0der@users.noreply.github.com>
2023-05-29 14:02:22 +0200

    logout on 401

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -4,7 +4,21 @@ import {serverAPIBaseURL} from '../config';
 class Api {
   axios = axios.create({
     baseURL: serverAPIBaseURL || 'http://localhost:4000/api/v1'
-  })
+  });
+
+  constructor() {
+    this.axios.interceptors.response.use(
+      res => res,
+      err => {
+        if (err.response.status === 401) {
+          localStorage.clear();
+          window.location.reload();
+        }
+
+        return Promise.reject(err);
+      }
+    );
+  }
 }
 
 export {
