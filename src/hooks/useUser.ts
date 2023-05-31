c6ba08b6b4ed599b64350f4a44df8eb6cfd06a23
G-C0der <31240102+G-C0der@users.noreply.github.com>
2023-05-31 16:14:48 +0200

    useUser hook added

---
+++ src/hooks/useUser.ts
@@ -0,0 +1,25 @@
+import {UserForm} from "../types";
+import {getErrorMessage, toApiResponse} from "./utils";
+import userApi from "../services/api/UserApi";
+
+const useUser = () => {
+  const register = async (userFormData: UserForm) => {
+    try {
+      const success = await userApi.register(userFormData);
+
+      if (!success) throw new Error('Failed to register');
+
+      return toApiResponse(true);
+    } catch (err: any) {
+      return toApiResponse(false, getErrorMessage(err))
+    }
+  };
+
+  return {
+    register
+  }
+};
+
+export {
+  useUser
+};
