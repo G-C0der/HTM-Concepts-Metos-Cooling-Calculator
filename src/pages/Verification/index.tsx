df7287f0635fa2f92cbd8ae8ef1352e5349a04f4
G-C0der <31240102+G-C0der@users.noreply.github.com>
2023-06-02 21:50:46 +0200

    verification page complete error messages

--- src/pages/Verification/index.tsx
+++ src/pages/Verification/index.tsx
@@ -1,17 +1,22 @@
 import React, {useContext, useEffect, useState} from 'react';
-import { CircularProgress, Paper, Typography, Alert, Box } from '@mui/material';
+import {CircularProgress, Paper, Typography, Alert, Box, Button, TextField} from '@mui/material';
 import { Grid } from "@mui/material";
 import {useParams} from "react-router-dom";
 import {UserContext} from "../../contexts";
 import {htmConceptsEmail} from "../../config";
 
-const Verification = (props: any) => {
+const specificIncompleteErrors = {
+  verificationLinkExpired: 'Your verification link has expired.'
+};
+
+const Verification = () => {
   const [status, setStatus] = useState('loading');
-  const [error, setError] = useState('');
+  const [error, setError] = useState<string | React.ReactNode>('');
+  const [email, setEmail] = useState('');
 
   const { token } = useParams();
 
-  const { verify } = useContext(UserContext);
+  const { verify, sendVerificationEmail } = useContext(UserContext);
 
   useEffect(() => {
     const verifyUser = async () => {
@@ -21,12 +26,44 @@ const Verification = (props: any) => {
         setStatus('success');
       } else {
         setStatus('error');
-        setError(verifyResponse.error!);
+        setError(completeError(verifyResponse.error!)!);
       }
     };
 
     verifyUser();
   }, [token]);
+
+  const completeError = (error: string) => {
+    if (!Object.values(specificIncompleteErrors).includes(error)) return (
+      <>{error} If you need support, you can contact us <a href={`mailto:${htmConceptsEmail}`}>here</a>.</>
+    );
+
+    switch (error) {
+      case specificIncompleteErrors.verificationLinkExpired:
+        return (
+          <>
+            {error}
+            To send a new verification email, enter the email associated with your account and click the button
+            below.<br/>
+            <TextField
+              label="Email"
+              type="email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              sx={{ mt: 2, mb: 1 }}
+            />
+            <Button
+              style={{backgroundColor: "#4CAF50", color: "#fff", border: "none", padding: "5px 10px 5px",
+                textAlign: "center", textDecoration: "none", display: "inline-block", fontSize: "12px",
+                margin: "30px 0 0 15px", cursor: "pointer"}}
+              onClick={async () => await sendVerificationEmail(email)}
+            >
+              Send Verification Email
+            </Button>.
+          </>
+        );
+    }
+  };
 
   return (
     <Grid container justifyContent="center">
@@ -45,18 +82,14 @@ const Verification = (props: any) => {
           {
             status === 'success' &&
             <>
-              <Alert severity="success" sx={{ mb: 2 }}>
-                <Typography variant="h5" align="center">
-                  Your account has been verified successfully.
-                </Typography>
+              <Alert severity="success" sx={{ mb: 1 }}>
+                Your account has been verified successfully.
               </Alert>
               <Alert severity="info">
-                <Typography variant="body1">
-                  Please note that your user account is currently inactive.
-                  We will review the provided data and email you, once your user account has been activated.
-                  We aim to complete the activation as soon as possible. If you do not receive an email from us or have any
-                  questions, you can contact us <a href={`mailto:${htmConceptsEmail}`}>here</a>.
-                </Typography>
+                Please note that your user account is currently inactive.
+                We will review the provided data and email you, once your user account has been activated.
+                We aim to complete the activation as soon as possible. If you do not receive an email from us or have any
+                questions, you can contact us <a href={`mailto:${htmConceptsEmail}`}>here</a>.
               </Alert>
             </>
           }
