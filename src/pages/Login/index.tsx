0bd19f843a7726435a226bb9c7ed8793af6b2fc5
G-C0der <31240102+G-C0der@users.noreply.github.com>
2023-06-02 13:23:10 +0200

    on login show complete error messages when not yet activated or verified and show send email verification button and contact link

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,16 +1,52 @@
 import React, {useContext, useState} from 'react';
 import {Alert, Button, Grid, Paper, TextField, Typography} from "@mui/material";
-import {AuthContext} from "../../contexts";
+import {AuthContext, UserContext} from "../../contexts";
 import {useNavigate} from "react-router-dom";
+import {htmConceptsEmail} from "../../config";
+
+const incompleteErrors = {
+  userAccountNotYetVerified: 'Your user account hasn\'t been verified yet.',
+  userAccountNotYetActivated: 'Your user account is currently inactive.'
+};
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [error, setError] = useState<string | React.ReactNode>('');
 
   const { login } = useContext(AuthContext);
+  const { sendVerificationEmail } = useContext(UserContext);
 
   const navigate = useNavigate();
+
+  const completeError = (error: string) => {
+    if (!Object.values(incompleteErrors).includes(error)) return error;
+
+    switch (error) {
+      case incompleteErrors.userAccountNotYetVerified:
+        return (
+          <>
+            {error} Please click on the "Verify Account" button in the verification email you have got after
+            your registration. If you need a new verification email,
+            <Button
+              style={{backgroundColor: "#4CAF50", color: "#fff", border: "none", padding: "0 10px",
+                textAlign: "center", textDecoration: "none", display: "inline-block", fontSize: "12px",
+                margin: "0 0 0 3px", cursor: "pointer"}}
+              onClick={async () => await sendVerificationEmail(email)}
+            >
+              click here
+            </Button>.
+          </>
+        );
+      case incompleteErrors.userAccountNotYetActivated:
+        return (
+          <>
+            {error} We're reviewing your user account and email you, once your user account has been activated.
+            If you need further information, you can contact us <a href={`mailto:${htmConceptsEmail}`}>here</a>
+          </>
+        );
+    }
+  };
 
   const handleSubmit = async (event: any) => {
     event.preventDefault();
@@ -18,7 +54,7 @@ const Login = () => {
     const loginResponse = await login({ email, password });
 
     if (loginResponse.success) navigate('/');
-    else setError(loginResponse.error!);
+    else setError(completeError(loginResponse.error!)!);
   };
 
   return (
