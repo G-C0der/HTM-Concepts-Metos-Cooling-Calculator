085d13ad0c3244f95de50323ded68c1b795a4b2d
David Burgherr <burghda@gmail.com>
2023-06-12 12:02:56 -0700

    use box and export isMobile

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -24,6 +24,7 @@ import htmConceptsLogo from '../../assets/img/HTM_Concepts_AG_Logo_mit_Claim_201
 import metosLogo from '../../assets/img/metos_logo.png';
 import { CustomAppBar } from "../../components/CustomAppBar";
 import { AuthContext } from "../../contexts";
+import { isMobile } from "../../utils/isMobile";
 
 const Home = () => {
   const [kettleCount, setKettleCount] = useState<KettleCount>(1);
@@ -119,14 +120,6 @@ const Home = () => {
     setConsumptionResult(consumptionResult);
   };
 
-  const isMobile = () => {
-    let check = false;
-    ((a) => {
-      if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
-    })(navigator.userAgent || navigator.vendor);
-    return check;  
-  }
-
   return loading
     ? (
       <Box style={{
@@ -138,23 +131,23 @@ const Home = () => {
         <CircularProgress size={80} />
       </Box>
     ) : (
-      <div className="Home">
-        <div className="Home-header">
+      <Box className="Home">
+        <Box className="Home-header">
           <CustomAppBar user={user!} />
-        </div>
+        </Box>
 
-        <div className="Home-content">
-          <div className="logo-container">
-            <div>
+        <Box className="Home-content">
+          <Box className="logo-container">
+            <Box>
               <img className='logo' src={htmConceptsLogo} width={100} />
-            </div>
+            </Box>
 
-            <div>
+            <Box>
               <img className='logo' src={metosLogo} width={100} />
-            </div>
-          </div>
+            </Box>
+          </Box>
 
-          <div className='form-container'>
+          <Box className='form-container'>
             <WaterForm
               tapWaterCoolingEntity={tapWaterCoolingEntity}
               waterLitreCHF={waterLitreCHF}
@@ -176,25 +169,25 @@ const Home = () => {
             />
 
 
-          </div>
+          </Box>
 
-          <div className='recommendation-container'>
+          <Box className='recommendation-container'>
             <C5iRecommendationsDataGrid rows={calculator.calculateC5iRecommendationsRows()} />
-          </div>
+          </Box>
 
           <ConsumptionDisplay
             consumptionResult={consumptionResult}
           />
 
-          <div className={"calculate-container-" + (isMobile() ? 'mobile' : 'desktop')}>
+          <Box className={"calculate-container-" + (isMobile() ? 'mobile' : 'desktop')}>
 
-            <div className='grid-button-container'>
-              <div className='data-grid'>
+            <Box className='grid-button-container'>
+              <Box className='data-grid'>
                 <TimePowerDataGrid rows={timePowerUsageRows} iceWaterCoolingEntity={iceWaterCoolingEntity} />
-              </div>
-              <div className='button-container'>
+              </Box>
+              <Box className='button-container'>
 
-                <div className='kettle-button'>
+                <Box className='kettle-button'>
                   <Tooltip title='add kettle'>
                     <Button
                       style={{
@@ -206,9 +199,9 @@ const Home = () => {
                       onClick={handleAddKettleClick}
                     ><AddIcon /></Button>
                   </Tooltip>
-                </div>
+                </Box>
 
-                <div className='calculate-button'>
+                <Box className='calculate-button'>
                   <Tooltip title='calculate'>
                     <Button
                       style={{
@@ -220,19 +213,19 @@ const Home = () => {
                       onClick={handleCalculateClick}
                     ><CalculateIcon /></Button>
                   </Tooltip>
-                </div>
-              </div>
-            </div>
+                </Box>
+              </Box>
+            </Box>
 
-            <div className='kettle-container'>
+            <Box className='kettle-container'>
               <KettleContainer
                 kettleEntities={kettleEntities}
                 handleKettleDeleteClick={handleKettleDeleteClick}
               />
-            </div>
+            </Box>
 
-          </div>
-        </div>
+          </Box>
+        </Box>
 
         {/*<Grid container sx={{ gap: 50, mt: 10, ml: 3, mr: 0 }}>*/}
         {/*  <Grid item xs={12} md={2}>*/}
@@ -257,7 +250,7 @@ const Home = () => {
         {/*    }*/}
         {/*  </Grid>*/}
         {/*</Grid>*/}
-      </div>
+      </Box>
     );
 };
 
