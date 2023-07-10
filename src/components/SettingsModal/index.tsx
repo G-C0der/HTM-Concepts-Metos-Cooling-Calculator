82c4dbe0abc3128ed72e18874ddaa9ec66f26d1c
G-C0der <31240102+G-C0der@users.noreply.github.com>
2023-07-10 11:01:32 +0200

    app version displayed in settings

--- src/components/SettingsModal/index.tsx
+++ src/components/SettingsModal/index.tsx
@@ -3,6 +3,7 @@ import './style.css';
 import {Dialog, DialogContent, Button, Typography, Box} from "@mui/material";
 import EditIcon from '@mui/icons-material/Edit';
 import LockResetIcon from '@mui/icons-material/LockReset';
+const packageJson = require('../../../package.json');
 
 interface SettingsModalProps {
   isOpen: boolean;
@@ -16,28 +17,45 @@ const SettingsModal = ({ isOpen, setIsOpen }: SettingsModalProps) => {
       onClose={() => setIsOpen(false)}
       PaperProps={{ className: 'settings-dialog-paper' }}
     >
-      <DialogContent>
-        <Typography variant="h5" align="center" component='h1' gutterBottom>
-          Settings
+      <DialogContent
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          height: '100%'
+        }}
+      >
+        <Box>
+          <Typography variant="h5" align="center" component='h1' gutterBottom>
+            Settings
+          </Typography>
+
+          <Button
+            fullWidth
+            sx={{ justifyContent: 'flex-start', textTransform: 'none', paddingLeft: '26px' }}
+            onClick={() => {}}
+          >
+            <EditIcon />
+            <Box sx={{ ml: 1 }}>Edit Profile</Box>
+          </Button>
+
+          <Button
+            fullWidth
+            sx={{ justifyContent: 'flex-start', textTransform: 'none', paddingLeft: '26px' }}
+            onClick={() => {}}
+          >
+            <LockResetIcon />
+            <Box sx={{ ml: 1 }}>Reset Password</Box>
+          </Button>
+        </Box>
+
+        <Typography
+          align="center"
+          gutterBottom
+          sx={{ fontSize: "15px" , padding: "0 15px 10px 15px", color: 'grey' }}
+        >
+          v{packageJson.version}
         </Typography>
-
-        <Button
-          fullWidth
-          sx={{ justifyContent: 'flex-start', textTransform: 'none', paddingLeft: '26px' }}
-          onClick={() => {}}
-        >
-          <EditIcon />
-          <Box sx={{ ml: 1 }}>Edit Profile</Box>
-        </Button>
-
-        <Button
-          fullWidth
-          sx={{ justifyContent: 'flex-start', textTransform: 'none', paddingLeft: '26px' }}
-          onClick={() => {}}
-        >
-          <LockResetIcon />
-          <Box sx={{ ml: 1 }}>Reset Password</Box>
-        </Button>
       </DialogContent>
     </Dialog>
   );
