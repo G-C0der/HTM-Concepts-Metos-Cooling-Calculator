e4206d037b2f522c40cd9059468ca9c31a349399
G-C0der <31240102+G-C0der@users.noreply.github.com>
2023-07-14 17:17:25 +0200

    cleanup

--- src/constants/message.ts
+++ src/constants/message.ts
@@ -3,7 +3,7 @@ const messageKeywords = {
 };
 
 const checkSpamFolderMessage = 'If you don\'t see it in your inbox, also check your spam folder.';
-const supportContactMessage = `If you need support you can contact us ${messageKeywords.here}.`;
+const supportContactMessage = `If you need support, you can contact us ${messageKeywords.here}.`;
 
 export {
   messageKeywords,
